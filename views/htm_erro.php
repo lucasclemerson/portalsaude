89742c57dc4d22d73cc59af346d526aec25b8392
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2022-06-04 22:41:21 -0300

    view do erro 404 70%

---
+++ views/htm_erro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php
+        require_once ('htm_meta.php');
+    ?>
+</head>
+<body>
+    <?php include_once('htm_modal.php'); ?>
+</body>
+</html>
+
+
+
+
+
