28711c5014e77ecadb7da34b456886d2c6fc0ed7
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2022-06-04 22:41:47 -0300

    metas padrão de todas as páginas

---
+++ views/htm_meta.php
@@ -0,0 +1,10 @@
+<meta http-equiv="Content-Type" charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Página não encontrada- <?=$base['titulo_pagina']?></title>
+<link rel="shortcut icon" href="<?=$base['favicon'];?>">
+<meta name="description" content="<?=$base['descricao']?>" />
+<meta property="og:description" content="<?=$base['descricao']?>">
+<meta name="author" content="<?=AUTOR?>">
+<meta name="classification" content="Website" />
+<meta name="robots" content="index, follow">
+<meta name="Indentifier-URL" content="<?=DOMINIO?>" />
